2c4f4c06587732a9c5df0076d1518a97b9e46111
Sean Dague <sdague@linux.vnet.ibm.com>
2013-06-25 18:54:57 -0400

    some work in progress on the hacky script

--- tempest.py
+++ tempest.py
@@ -8,6 +8,27 @@ es = ElasticSearch(ES_URL)
 
 # '{"query": { "match": {"@message": {"query": "FAILED SKIP failures", "operator": "and"}}
 
+tempest_failed_jobs = {
+    "query": {
+        "query_string": {
+            "query": ('@tags:"console.html" AND ( @message:'
+                      '"Finished: SUCCESS" OR @message:"Finished: FAILURE" )')
+            }
+        }
+    }
+
+    #     "match": {
+    #         "@fields.build_status": {
+    #             "query": "FAILURE"
+    #             },
+    #         "@message": "Finished: FAILURE"{
+    #             "query": "Finished: FAILURE",
+    #             }
+    #         }
+    #     }
+    # }
+
+
 tempest_failed_log = {
     "query": {
         "match": {
@@ -18,6 +39,7 @@ tempest_failed_log = {
             }
         }
     }
+
 
 
 print es.search(tempest_failed_log)
